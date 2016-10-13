543323181fa38624fcf09a74335e5008aaf60612
Jarvanerp <jarno@thehyve.nl>
2016-10-13 17:22:51 +0200

    Add arguments for CLUC study data export

--- grails-app/controllers/org/transmartproject/rest/ExportController.groovy
+++ grails-app/controllers/org/transmartproject/rest/ExportController.groovy
@@ -19,23 +19,30 @@ class ExportController {
 
     def export(ExportCommand exportCommand) {
         //Used for retrieving the resultinstanceIds
-        //def yesterday = new Date() -1
-        //def result = QtQueryResultInstance.findAllByStartDateGreaterThan(yesterday )
-        //render(result)
-        def arguments = [
+        def yesterday = new Date() -1
+        def result = QtQueryResultInstance.findAllByStartDateGreaterThan(yesterday )
+        render(result)
+        def arguments_CLUC = [
+                conceptKeys: ["Agilent miRNA microarray": "\\\\Public Studies\\Public Studies\\CLUC\\Molecular profiling\\High-throughput molecular profiling\\Expression (miRNA)\\Agilent miRNA microarray\\",
+                              "Demographics":"\\\\Public Studies\\Public Studies\\CLUC\\Characteristics\\",
+                              "MZ ratios":"\\\\Public Studies\\Public Studies\\CLUC\\Molecular profiling\\High-throughput molecular profiling\\Expression (protein)\\LC-MS-MS\\Protein level\\TPNT\\MZ ratios\\" ],
+                resultInstanceIds: [28742, 28743]
+        ]
+        def arguments_GSE37427= [
                 conceptKeys: ["MET998": "\\\\Public Studies\\Public Studies\\GSE37427\\Biomarker Data\\MET998\\",
                               "Demographics":"\\\\Public Studies\\Public Studies\\GSE37427\\Demographics\\",
                               "Trial Arm": "\\\\Public Studies\\Public Studies\\GSE37427\\Demographics\\Trial Arm\\",
-                             "Control" : "\\\\Public Studies\\Public Studies\\GSE37427\\Demographics\\Trial Arm\\Control\\"],
-                            //"Human": "\\\\Public Studies\\Public Studies\\GSE37427\\Biomarker Data\\MET998\\Human\\"],
+                              "Control" : "\\\\Public Studies\\Public Studies\\GSE37427\\Demographics\\Trial Arm\\Control\\"],
+                //"Human": "\\\\Public Studies\\Public Studies\\GSE37427\\Biomarker Data\\MET998\\Human\\"],
                 resultInstanceIds: [28741, 28740]
         ]
         throwIfInvalid exportCommand
         def filesList = []
-        arguments.resultInstanceIds.each { it ->
-            def files = restExportService.export(conceptKeys: arguments.conceptKeys, resultInstanceIds: [it])
-            files = restExportService.parseFiles(files)
-            filesList.add(files)
+        arguments_CLUC.resultInstanceIds.each { it ->
+            def files1 = restExportService.export(conceptKeys: arguments_CLUC.conceptKeys, resultInstanceIds: [it])
+            //def files2 = restExportService.parseFiles(files1)
+            filesList += files1
+            //filesList.add(files2)
         }
         File zipFile = restExportService.createZip(filesList)
         sendFileService.sendFile servletContext, request, response, zipFile
