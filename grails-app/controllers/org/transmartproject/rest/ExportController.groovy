dea62316cd5312272f9cdfa2d9889167240fb43a
Jarvanerp <jarno@thehyve.nl>
2016-10-28 12:37:14 +0200

    Add working functional test for datatype rest api call

--- grails-app/controllers/org/transmartproject/rest/ExportController.groovy
+++ grails-app/controllers/org/transmartproject/rest/ExportController.groovy
@@ -1,12 +1,13 @@
 package org.transmartproject.rest
 
 import grails.validation.Validateable
+import groovy.json.JsonException
+import groovy.json.JsonSlurper
+import org.apache.commons.lang.NullArgumentException
 import org.springframework.beans.factory.annotation.Autowired
 import org.transmartproject.core.exceptions.InvalidArgumentsException
-import org.transmartproject.core.ontology.OntologyTerm
 import org.transmartproject.db.RestExportService
 import org.transmartproject.db.querytool.QtQueryResultInstance
-import org.transmartproject.rest.misc.JsonParametersParser
 
 import java.nio.file.Path
 
@@ -19,10 +20,12 @@ class ExportController {
 
     def sendFileService
 
-    def conceptsResourceService
-
-    def export() {
-        //throwIfInvalid exportCommand
+    /**POST request on /export/
+     *  Returns a zipfile containing the data from the selected cohort.
+     *
+     */
+    def export(ExportCommand exportCommand) {
+        throwIfInvalid exportCommand
         //TODO: IDEA restExportService.export() returns list with results object instead of files.
         def arguments = retrieveArguments()
         Path tmpPath = restExportService.createTmpDir()
@@ -35,26 +38,35 @@ class ExportController {
         sendFileService.sendFile servletContext, request, response, zipFile
     }
 
+    /**GET request on /export/datatypes
+     *  Returns datatypes and patient number of given concepts.
+     *
+     */
     def datatypes(){
-        //Retrieve datatype
-        //Get total number of unique patients belonging to the concepts which have the datatype.
-        def concept_arguments = retrieveArguments()
-        List datatypes = []
-        concept_arguments.each { it ->
-            List cohortDataTypes = []
-            it.conceptKeys.each { conceptKey ->
-                Map datatypeMap = [:]
-                OntologyTerm concept = conceptsResourceService.getByKey(conceptKey)
-                def datatype = restExportService.getHighDimMetaData(concept)
-                datatypeMap['conceptKey'] = conceptKey
-                datatypeMap['dataType'] = datatype.get('dataTypes')[0]
-                datatypeMap['numOfPatients'] = 10
-                cohortDataTypes.add(datatypeMap)
-            }
-            datatypes.add(cohortDataTypes)
+        def jsonSlurper = new JsonSlurper()
+        if (!(params.containsKey('concepts'))){
+            throw new NoSuchElementException(
+                    "No parameter named concepts."
+            )
         }
-        print(datatypes)
-        respond(datatypes)
+        def test = params.get('concepts').decodeURL()
+        try {
+            def concept_arguments = jsonSlurper.parseText(test)
+            if (concept_arguments==null){
+                throw new NullArgumentException(
+                        "Parameter concepts has no value."
+                )
+            }
+            List datatypes = []
+            concept_arguments.each { it ->
+                List conceptKeysList = it.conceptKeys
+                datatypes += restExportService.getDataTypes(conceptKeysList)
+            }
+            respond(restExportService.formatDataTypes(datatypes))
+        } catch(JsonException e){
+            "Given value was non valid JSON."
+        }
+
     }
 
     private void throwIfInvalid(command) {
@@ -66,81 +78,6 @@ class ExportController {
         }
     }
 
-    def retrieveArguments(){
-        //Used for retrieving the resultinstanceIds
-        //def yesterday = new Date() -1
-        //def result = QtQueryResultInstance.findAllByStartDateGreaterThan(yesterday )
-        //render(result)
-        def arguments_GSE37427_merged= [[
-                "conceptKeys": ["MET998": "\\\\Public Studies\\Public Studies\\GSE37427\\Biomarker Data\\MET998\\",
-                              "Demographics":"\\\\Public Studies\\Public Studies\\GSE37427\\Demographics\\",
-                              "Trial Arm": "\\\\Public Studies\\Public Studies\\GSE37427\\Demographics\\Trial Arm\\",
-                              "Control" : "\\\\Public Studies\\Public Studies\\GSE37427\\Demographics\\Trial Arm\\Control\\"],
-                "resultInstanceIds": [28741, 28740],
-                "exportDataFormat": ["tsv"]
-        ]]
-        def arguments_two_studies_merged =[[ conceptKeys: [
-                "Biomarker Data":"\\\\Public Studies\\Public Studies\\GSE8581\\Biomarker Data\\",
-                "LOG":"\\\\Public Studies\\Public Studies\\GSE8581\\Biomarker Data\\GPL570_BOGUS\\LOG\\",
-                "Human":"\\\\Public Studies\\Public Studies\\GSE37427\\Biomarker Data\\MET998\\Human\\"],
-                                         resultInstanceIds: [28750, 28751],
-                                         exportDataFormat: ['tsv']]]
-        def arguments_two_studies_separated = [[ conceptKeys: [
-                "Biomarker Data":"\\\\Public Studies\\Public Studies\\GSE8581\\Biomarker Data\\",
-                "GPL570_BOGUS":"\\\\Public Studies\\Public Studies\\GSE8581\\Biomarker Data\\GPL570_BOGUS\\"],
-                                             resultInstanceIds: [28750],
-                                             exportDataFormat: ['tsv']],
-                [ conceptKeys: [
-                                "Human":"\\\\Public Studies\\Public Studies\\GSE37427\\Biomarker Data\\MET998\\Human\\",
-                                "Biomarker Data":"\\\\Public Studies\\Public Studies\\GSE37427\\Biomarker Data\\",
-                "Demographics":"\\\\Public Studies\\Public Studies\\GSE37427\\Demographics\\"],
-                 resultInstanceIds: [28751],
-                 exportDataFormat: ['tsv']
-        ]]
-        def arguments_CLUC_separated =
-                [[
-                         "resultInstanceIds": [28742],
-                         "conceptKeys": [
-                                 "Agilent miRNA microarray": "\\\\Public Studies\\Public Studies\\CLUC\\Molecular profiling\\High-throughput molecular profiling\\Expression (miRNA)\\Agilent miRNA microarray\\",
-                                 "Demographics": "\\\\Public Studies\\Public Studies\\CLUC\\Characteristics\\",
-                                 "MZ ratios": "\\\\Public Studies\\Public Studies\\CLUC\\Molecular profiling\\High-throughput molecular profiling\\Expression (protein)\\LC-MS-MS\\Protein level\\TPNT\\MZ ratios\\"
-                         ],
-                         "exportDataFormat": ["csv", "tsv"]
-                 ], [
-                         "resultInstanceIds": [28743],
-                         "conceptKeys": [
-                                 "Agilent miRNA microarray": "\\\\Public Studies\\Public Studies\\CLUC\\Molecular profiling\\High-throughput molecular profiling\\Expression (miRNA)\\Agilent miRNA microarray\\",
-                                 "Demographics": "\\\\Public Studies\\Public Studies\\CLUC\\Characteristics\\",
-                                 "MZ ratios": "\\\\Public Studies\\Public Studies\\CLUC\\Molecular profiling\\High-throughput molecular profiling\\Expression (protein)\\LC-MS-MS\\Protein level\\TPNT\\MZ ratios\\"
-                         ],
-                         "exportDataFormat": ["tsv"]
-                 ]]
-        def arguments_CLUC_merged =
-                [[
-                         "resultInstanceIds": [28742, 28743],
-                         "conceptKeys": [
-                                 "Agilent miRNA microarray": "\\\\Public Studies\\Public Studies\\CLUC\\Molecular profiling\\High-throughput molecular profiling\\Expression (miRNA)\\Agilent miRNA microarray\\",
-                                 "Demographics": "\\\\Public Studies\\Public Studies\\CLUC\\Characteristics\\",
-                                 "MZ ratios": "\\\\Public Studies\\Public Studies\\CLUC\\Molecular profiling\\High-throughput molecular profiling\\Expression (protein)\\LC-MS-MS\\Protein level\\TPNT\\MZ ratios\\"
-                         ],
-                         "exportDataFormat": ["csv", "tsv"]
-                 ]]
-
-        def highdim_datatypes =
-                [["conceptKeys": [
-                        "\\\\Public Studies\\Public Studies\\CLUC\\Molecular profiling\\",
-                        "\\\\Public Studies\\Public Studies\\CLUC\\Molecular profiling\\High-throughput molecular profiling\\Expression (protein)\\LC-MS-MS\\Protein level\\TPNT\\MZ ratios\\"
-                ]],
-                 ["conceptKeys": [
-                         "\\\\Public Studies\\Public Studies\\GSE37427\\Biomarker Data\\",
-                         "\\\\Public Studies\\Public Studies\\GSE37427\\Biomarker Data\\MET998\\Human\\"]
-                ]]
-
-        //def argumentsJSON = JsonOutput.toJson(arguments_CLUC_separated)
-        //def jsonSlurper = new JsonSlurper()
-        //def arguments = jsonSlurper.parseText(argumentsJSON)
-        highdim_datatypes
-    }
 
 
 }
@@ -153,3 +90,4 @@ class ExportCommand {
         arguments nullable: false
     }
 }
+
